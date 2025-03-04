6d45957b1fc1de085915ba06cff9a8e462ba3fc3
irabeson <iohannrabeson@users.noreply.github.com>
2025-03-03 22:59:55 -0500

    Clippy

--- src/audio.rs
+++ src/audio.rs
@@ -258,7 +258,7 @@ mod details {
             self.sender
                 .try_send(Message::Visualization(visualization::Message::AudioBuffer(
                     self.source.channels(),
-                    self.buffer.iter().map(|sample| *sample).collect(),
+                    self.buffer.to_vec(),
                 )))
                 .unwrap();
 
